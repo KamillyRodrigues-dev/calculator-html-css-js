54ff92713db320ab3507a6cbc6c622e3b61d9cf2
Kamilly Vitoria Rodrigues Silva <kamillyr93@gmail.com>
2026-08-25 12:56:38 -0300

    feat: calcula todas as operações

--- js/script.js
+++ js/script.js
@@ -11,4 +11,12 @@ function back(){
     display.value = display.value.slice(0, -1); // slice corta uma parte de uma string, um caracter
 }
 
- 
+function calculate(){
+    const display = document.getElementById('display')
+    try{
+        let expression = display.value.replace(/%/g, '/100'); // g -> global, substitui todas as ocorrencias inves de só a primeira
+        display.value = eval(expression);
+    } catch{
+        display.value = "Error"
+    }
+}
