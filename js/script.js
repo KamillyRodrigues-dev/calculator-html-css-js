30f28e10b009d7db734dd9a333361aade506550f
Kamilly Vitoria Rodrigues Silva <kamillyr93@gmail.com>
2026-08-25 12:35:29 -0300

    feat: deleta numero digitado um por um

--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ function insertToDisplay(data){
 function clean(){
     document.getElementById('display').value = ''
 }
+
+function back(){
+    const display =  document.getElementById('display')
+    display.value = display.value.slice(0, -1); // slice corta uma parte de uma string, um caracter
+}
+
+ 
